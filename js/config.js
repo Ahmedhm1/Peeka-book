15cfed895b9bfe59e848788a046e8b4fdabce9e2
Ahmed Hisham <139140231+Ahmedhm1@users.noreply.github.com>
2026-01-10 18:27:07 +0200

    Upload Files

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    API_BASE_URL: "https://haughtiest-dressier-cheyenne.ngrok-free.dev/api",
+    BACKEND_DASHBOARD_URL: "https://haughtiest-dressier-cheyenne.ngrok-free.dev/dashboard"
+};
